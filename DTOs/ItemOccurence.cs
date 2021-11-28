86d0991cd636f0615a72a5f50790b6583860b461
Oscar Gürtler <oscarguertler@gmail.com>
2021-11-28 12:51:30 +0100

    Projektdateien hinzufügen.

---
+++ DTOs/ItemOccurence.cs
@@ -0,0 +1,15 @@
+﻿namespace dope_stats.DTOs
+{
+    public class ItemOccurence
+    {
+        public Dictionary<string, int>? clothes { get; set; }
+        public Dictionary<string, int>? foot { get; set; }
+        public Dictionary<string, int>? hand { get; set; }
+        public Dictionary<string, int>? drugs { get; set; }
+        public Dictionary<string, int>? neck { get; set; }
+        public Dictionary<string, int>? ring { get; set; }
+        public Dictionary<string, int>? waist { get; set; }
+        public Dictionary<string, int>? weapon { get; set; }
+        public Dictionary<string, int>? vehicle { get; set; }
+    }
+}
---
+++ DTOs/ItemRarity.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace dope_stats.DTOs
+{
+    public class ItemRarity
+    {
+        public Dictionary<string, double>? clothes { get; set; }
+        public Dictionary<string, double>? foot { get; set; }
+        public Dictionary<string, double>? hand { get; set; }
+        public Dictionary<string, double>? drugs { get; set; }
+        public Dictionary<string, double>? neck { get; set; }
+        public Dictionary<string, double>? ring { get; set; }
+        public Dictionary<string, double>? waist { get; set; }
+        public Dictionary<string, double>? weapon { get; set; }
+        public Dictionary<string, double>? vehicle { get; set; }
+    }
+}
